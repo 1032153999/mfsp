d56d0c641922c6214b5850c3afc1cf104609a37e
Default Name <1032153999@qq.com>
2020-11-23 16:30:48 +0800

    html
    Signed-off-by: Default Name <1032153999@qq.com>

--- src/main/java/com/example/mfsp/controller/updateOrderController.java
+++ src/main/java/com/example/mfsp/controller/updateOrderController.java
@@ -1,12 +1,16 @@
 package com.example.mfsp.controller;
 
 
+import com.example.mfsp.entity.Orderform;
 import com.example.mfsp.service.orderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class updateOrderController {
@@ -15,7 +19,15 @@ public class updateOrderController {
     private orderService orderService;
     @RequestMapping(value="/updateOrder",method= RequestMethod.GET)
     @ResponseBody
-    public String updateOrder(){
+    public String updateOrder(Orderform orderform){
+        Orderform orderFormVo= new Orderform();
+        orderFormVo.setOrderformid(orderform.getOrderformid());
+        List<Orderform> orderforms=new ArrayList<>();
+        orderforms=orderService.selectAll(orderFormVo);
+        System.out.println("orderforms.get(0)"+orderforms.get(0).getOrderformaddress());
+        orderService.updateByExampleSelective(orderforms.get(0),orderform);
+
+        System.out.println(orderform.getOrderformid()+"-----"+orderform.getUserid());
 
         return " ";
     }

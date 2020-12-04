5beaa01f22bd1eec21386e7578b4688fddb6fe3e
crc-java <1104603939@qq.com>
2020-12-04 14:43:25 +0800

    删除购物车功能

---
+++ src/main/java/com/example/mfsp/controller/Deleteshoppingcart.java
@@ -0,0 +1,43 @@
+package com.example.mfsp.controller;
+
+import com.example.mfsp.entity.Shoppingcart;
+import com.example.mfsp.service.shoppingcartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class Deleteshoppingcart {
+
+    @Autowired
+    private com.example.mfsp.service.shoppingcartService shoppingcartService;
+
+
+    //删除单一商品的购物车
+    @RequestMapping(value = "deleteone")
+    @ResponseBody
+    public String deleteshoppingcart(Shoppingcart shoppingcart) {
+        if (shoppingcart.getShoppingcartid() != null) {
+            shoppingcartService.delete(shoppingcart);
+            return "";
+        } else {
+            return "";
+        }
+    }
+
+//删除所有购物车的商品
+    @RequestMapping(value = "deleteall")
+    @ResponseBody
+    public String deleteallshoppingcart(Shoppingcart shoppingcart){
+        if (shoppingcart.getShoppingcartid() != 0){
+            return "传值错误";
+        }else if(shoppingcart.getUserid() != 0){
+            shoppingcartService.delete(shoppingcart);
+            return "删除成功";
+        }else {
+            return "";
+        }
+    }
+
+
+}

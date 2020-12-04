f556abd6c8ad4625da889c0a0531a2ccd78577f6
Default Name <1032153999@qq.com>
2020-12-04 16:40:35 +0800

    index addShoppingCart,clothingDetail addshopingCart
    Signed-off-by: Default Name <1032153999@qq.com>

--- src/main/java/com/example/mfsp/controller/AddtoFavouritesController.java
+++ src/main/java/com/example/mfsp/controller/AddtoFavouritesController.java
@@ -24,25 +24,53 @@ public class AddtoFavouritesController {
     //,@RequestParam("id")  Integer id
 
 
+//    @GetMapping("Addtofavourties")
+//    @ResponseBody
+//    public String addtoFavourties(Favourites favourites,@RequestParam("id")  Integer id) {
+//
+//        Favourites favourites2 = new Favourites();
+//        favourites2.setUserid((id));
+//        List<Favourites> favourite2=new ArrayList<>();
+//        favourite2 =favouritesService.selectAll(favourites2);
+//        favourites.setClothingid(4);
+//        favourites.setFavouritesid(18);
+//        favourites.setUserid(4);
+//        System.out.println("设置成功");
+//        if (favourite2.size() == 1 ){
+//            System.out.println("该商品已存在收藏夹中");
+//
+//        }else {
+//            favouritesService.insert(favourites);
+//            System.out.println("添加成功");
+//        }
+//        return "success";
+//    }
+
     @GetMapping("Addtofavourties")
     @ResponseBody
-    public String addtoFavourties(Favourites favourites,@RequestParam("id")  Integer id) {
+    public Map<String, Object> addtoFavourties(Favourites favourites) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("code", 0);
 
         Favourites favourites2 = new Favourites();
-        favourites2.setUserid((id));
+        favourites2.setClothingid(favourites.getClothingid());
+        favourites2.setUserid(favourites.getUserid());
         List<Favourites> favourite2=new ArrayList<>();
         favourite2 =favouritesService.selectAll(favourites2);
-        favourites.setClothingid(4);
-        favourites.setFavouritesid(18);
-        favourites.setUserid(4);
+//        favourites.setClothingid(4);
+//        favourites.setFavouritesid(18);
+//        favourites.setUserid(4);
         System.out.println("设置成功");
         if (favourite2.size() == 1 ){
             System.out.println("该商品已存在收藏夹中");
+            result.put("msg", "该商品已存在收藏夹中");
+
 
         }else {
             favouritesService.insert(favourites);
             System.out.println("添加成功");
+            result.put("msg", "添加成功");
         }
-        return "success";
+        return result;
     }
 }
